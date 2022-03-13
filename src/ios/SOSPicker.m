34e32624b13902bb79ea6bb2811f68381d51c82f
Ori Yechieli <ori.yechieli@gmail.com>
2022-03-13 12:22:02 +0200

    LV-1290 package-lock.json (#1)
    * Add fullscreen functionality for iOS
    
    (cherry picked from commit b7b717cb7fe333dc396b43389528c22ff78387c7)
    
    * LV-1290 imagepicker package-lock.json, sync with Guy Nesher's ImagePicker
    
    Co-authored-by: Danny Kay <danny.kay@locusview.com>

--- src/ios/SOSPicker.m
+++ src/ios/SOSPicker.m
@@ -95,6 +95,10 @@ typedef enum : NSUInteger {
     picker.colsInLandscape = 6;
     picker.minimumInteritemSpacing = 2.0;
 
+    if (@available(iOS 13.0, *)) {
+        picker.modalPresentationStyle = UIModalPresentationFullScreen;
+    }
+
     if(!disable_popover) {
         picker.modalPresentationStyle = UIModalPresentationPopover;
 
